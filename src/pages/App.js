1b2081df8d227cadc7abfc5c9ba1aa3c8dca34ea
viniciustq14 <viniciustq14@bol.com.br>
2022-02-26 13:07:17 -0300

    feat(): Arquivos
    Faz a captura do arquivo
    Converte o arquivo para ObjectURL
    Reestrutura as paginas para classe

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,39 +1,63 @@
 /* eslint-disable jsx-a11y/alt-text */
 import '../css/App.css';
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import CallMadeIcon from '@material-ui/icons/CallMade';
 
-function App() {
-  return (
-    <div className="App">
-      <div className='main'>
-        <img
-          src="/Trollface.png"
-          className="logo"
-        ></img>
-        <h1>Meme Generator</h1>
+import MemeGenerate from '../service/MemeGenerate.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className='main'>
+          <img
+            src="/Trollface.png"
+            className="logo"
+          ></img>
+          <h1>Meme Generator</h1>
+        </div>
+  
+        <button className="button" onClick={this.getFile}>
+          Upload
+          <CloudUploadIcon className="icon"></CloudUploadIcon>
+        </button>
+
+        <input
+          type="file"
+          id="file"
+          accept="image/png, image/jpeg"
+          onChange={(event) => { this.setFile(event.target.files[0]) }}
+        />
+
+        <Link to="/edit" id="edit-page"/>
+
+        <footer>
+          <label>Desenvolvido por Os Brabos</label>
+          <a href="https://github.com/CarlosMendesDev" target="blank">
+            Visite meu GitHub
+            <CallMadeIcon></CallMadeIcon>
+          </a>
+        </footer>
       </div>
+    );
+  }
 
-      <button className="button" onClick={getFile}>
-        Upload
-        <CloudUploadIcon className="icon"></CloudUploadIcon>
-      </button>
-      <input type="file" id="file"></input>
-      <footer>
-        <label>Desenvolvido por Os Brabos</label>
-        <a href="https://github.com/CarlosMendesDev" target="blank">
-          Visite meu GitHub
-          <CallMadeIcon></CallMadeIcon>
-        </a>
-      </footer>
-    </div>
-  );
-}
+  getFile() {
+    const file = document.getElementById('file');
 
-const getFile = () => {
-  const file = document.getElementById('file');
+    file.click();
+  }
 
-  file.click();
+  setFile(file) {
+    const fileURL = window.URL.createObjectURL(file);
+    MemeGenerate.setFile(fileURL);
+    
+    const btnPageEdit = document.getElementById('edit-page');
+    btnPageEdit.click();
+  }
+
 }
 
 export default App;
--- src/pages/EditMeme.js
+++ src/pages/EditMeme.js
@@ -1,48 +1,56 @@
 /* eslint-disable jsx-a11y/alt-text */
 import '../css/EditMeme.css';
 import CallMadeIcon from '@material-ui/icons/CallMade';
+import { Component } from 'react';
+import { Link } from 'react-router-dom'
 
-function EditMeme() {
-  return (
-    <div className="EditMeme">
-      <header>
-        <img
-          src="/Trollface.png"
-          className="logo-header"
-        ></img>
-        <h1 className="title-header">Meme Generator</h1>
-      </header>
+import MemeGenerate from '../service/MemeGenerate';
 
-      <div className="container-edit">
-        <input
-          placeholder="Top text..."
-          type="text"
-        ></input>
-
-        <img
-          className="image-edit"
-          src="https://veja.abril.com.br/wp-content/uploads/2022/02/000_9XR6RW.jpg"
-        ></img>
-
-        <input
-          placeholder="Bottom text..."
-          type="text"
-        ></input>
-
-        <button className="button">
-          Generate
-        </button>
+class EditMeme extends Component {
+  render() {
+    return (
+      <div className="EditMeme">
+        <header>
+          <Link to="/">
+            <img
+              src="/Trollface.png"
+              className="logo-header"
+            ></img>
+          </Link>
+          <h1 className="title-header">Meme Generator</h1>
+        </header>
+  
+        <div className="container-edit">
+          <input
+            placeholder="Top text..."
+            type="text"
+          ></input>
+  
+          <img
+            className="image-edit"
+            src={ MemeGenerate.getFile() }
+          ></img>
+  
+          <input
+            placeholder="Bottom text..."
+            type="text"
+          ></input>
+  
+          <button className="button">
+            Generate
+          </button>
+        </div>
+  
+        <footer>
+          <label>Desenvolvido por Os Brabos</label>
+          <a href="https://github.com/CarlosMendesDev" target="blank">
+            Visite meu GitHub
+            <CallMadeIcon></CallMadeIcon>
+          </a>
+        </footer>
       </div>
-
-      <footer>
-        <label>Desenvolvido por Os Brabos</label>
-        <a href="https://github.com/CarlosMendesDev" target="blank">
-          Visite meu GitHub
-          <CallMadeIcon></CallMadeIcon>
-        </a>
-      </footer>
-    </div>
-  );
+    );
+  }
 }
 
 export default EditMeme;
